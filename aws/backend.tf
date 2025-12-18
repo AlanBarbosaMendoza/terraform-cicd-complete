4738f8d6c4837eea85a8d27e2ee685a71656074f
Ubuntu <ubuntu@ip-172-31-18-237.ec2.internal>
2025-12-18 20:41:48 +0000

    Change aws backend

--- aws/backend.tf
+++ aws/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "cisco-raman-terraform-state-bucket "
+    bucket         = "cisco-ricardo-terraform-state-bucket "
     key            = "ci/aws/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "cisco-raman-terraform-state-lock"
+    dynamodb_table = "cisco-ricardo-terraform-state-lock"
     encrypt        = true
   }
 }
