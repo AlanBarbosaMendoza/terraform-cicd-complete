fe28eb18c56cb1322d0d9034c0ef4fec007b8764
Raman Khanna <89792896+ramannkhanna2@users.noreply.github.com>
2025-12-19 21:44:08 +0530

    Update resource.tf

--- aws/resource.tf
+++ aws/resource.tf
@@ -12,7 +12,49 @@ resource "aws_s3_bucket" "demo" {
   }
 }
 
+
 resource "random_id" "suffix" {
   byte_length = 4
 }
 
+
+
+resource "aws_s3_bucket_public_access_block" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+
+resource "aws_s3_bucket_versioning" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+
+
+resource "aws_s3_bucket_lifecycle_configuration" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  rule {
+    id     = "basic-lifecycle"
+    status = "Enabled"
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    expiration {
+      days = 365
+    }
+  }
+}
